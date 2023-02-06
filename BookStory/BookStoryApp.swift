572de04dfc69e1112d26c7c70a4a7009f219012e
Jesus Sanz <jesus@jsanz.dev>
2023-02-06 19:09:35 +0100

    Initial Commit

---
+++ BookStory/BookStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoryApp.swift
+//  BookStory
+//
+//  Created by Jesus Sanz on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BookStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
